6d258f45caa79476508b35ad3e08cd70f50b5dd0
Sammie <Alphasam123@gmail.com>
2023-02-06 12:43:28 +0000

    update to new image

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const colour = '201, 235, 248';
-const fullImage = "url('whale.jpg')";
-const brushStrokeImage = "url('brushstroke.png')"
+const light = '119,169,222';
+const dark = '59,84,109';
+
+const fullImage = "url('whale.png')";
+const partialTransparent = "url('partialWhale.png')"
+const triangleTransparent = "url('triangleWhale.png')"
 
 const scroller = document.querySelector('.scroller');
 const headerSection = document.querySelector('.headerSection');
 
-scroller.onscroll = function () {
+scroller.onscroll = () => {
     const headerSectionHeight = headerSection.offsetHeight;
     const headerSectionPosition =
         headerSection.getBoundingClientRect().bottom;
 
-    const opac =
+    console.log(headerSectionHeight, headerSectionPosition)
+
+    const partialOpac =
         Math.max(headerSectionHeight - headerSectionPosition, 0) /
         headerSectionHeight;
 
+    const triangleOpac = partialOpac - 1;
+
     scroller.style.background =
-        `${brushStrokeImage} no-repeat center, `
-        + `linear-gradient(rgba(${colour}, `
-        + opac
-        + `), rgba(${colour}, `
-        + opac
-        + `)), ${fullImage} no-repeat center`;
+        `${triangleTransparent} no-repeat center, `
+        + `linear-gradient(rgba(${light}, `
+        + triangleOpac
+        + `), rgba(${dark}, `
+        + triangleOpac
+        + `)), `
+
+        + `${partialTransparent} no-repeat center, `
+        + `linear-gradient(rgba(${light}, `
+        + partialOpac
+        + `), rgba(${dark}, `
+        + partialOpac
+        + `)), `
+
+        + `${fullImage} no-repeat center`;
 };
